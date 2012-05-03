8d48464bc3f04c85937b1cdc0ab9415aa80fb885
Rohit R <rrohit.rht@gmail.com>
2012-05-04 01:44:29 +0530

    Code clean up. Timeout removed; use wget timeout instead

--- ffp-cli.py
+++ ffp-cli.py
@@ -20,13 +20,13 @@ def cb(option, opt_str, value, parser):
         args.extend(getattr(parser.values, option.dest))
     setattr(parser.values, option.dest, args)
 
-def test_dwnld(p, u, t, f):
+def test_dwnld(p, u, f):
     url = ""
     os.environ.update({'http_proxy':p})
     try:
         with open(os.devnull, 'wb') as devnull:
             start = time()
-            subprocess.check_call(['wget', '-T', str(t), "-t", str(1), 
+            subprocess.check_call(['wget', "-t", str(1), 
                                    "-O", f, u], stdout=devnull, 
                                   stderr=subprocess.STDOUT)
         end = time()
@@ -36,15 +36,20 @@ def test_dwnld(p, u, t, f):
         os.remove(f)
     return end - start
 
+def proxycmp(proxylist, url, filename):
+    tlist=[]
+    for proxy in proxylist:
+        td = test_dwnld(proxy, url, filename)
+        tlist.append((proxy, td))
+    tlist = sorted(tlist, key=lambda x:x[1])
+    return tlist
+    
 def main():
     usage = "./find-fatest-proxy.py [options] -p proxy1 proxy2 ..."
     parser = optparse.OptionParser(usage)
     parser.add_option("-u", "--url", dest="url",
                      help="use URL for comparisons",
                      type="string", default=DEFAULT_URL)
-    parser.add_option("-t", "--timeout", dest="timeout",
-                     help="timeout for individual attempt",
-                     type="int", default=5)
     parser.add_option("-f", "--filename", dest="filename",
                      help='''Name and optionally the full path to which to\
  download the test file. `tempdwldfile` by default.''',
@@ -59,29 +64,25 @@ compared")
                       help="Read proxies from a file. Default file is ~/.proxyfile")
     (options, args) = parser.parse_args()
     if options.proxyfile[0]=='~':
-        print "here"
         options.proxyfile = os.environ.get('HOME') + options.proxyfile.lstrip('~')
-        print options.proxyfile
     if not options.proxies and not os.path.isfile(options.proxyfile):
         parser.error("No proxies provided")
-    tlist = []
+    proxylist = []
     if options.proxies:
         for proxy in options.proxies:
-            td = test_dwnld(proxy, options.url, options.timeout, options.filename)
-            tlist.append((proxy, td))
+            proxylist.append(proxy)
     if os.path.isfile(options.proxyfile):
         pf = open(options.proxyfile)
         proxies = pf.readlines()
         for proxy in proxies:
-            proxy=proxy.strip()
-            td = test_dwnld(proxy, options.url, options.timeout, options.filename)
-            tlist.append((proxy, td))
+            proxy = proxy.strip()
+            proxylist.append(proxy)
         pf.close()
-    tlist = sorted(tlist, key=lambda x:x[1])
+    tl = proxycmp(proxylist, options.url, options.filename)
     if not options.listproxies:
-	    print tlist[0][0]
+	    print tl[0][0]
     else:
-	    for i in tlist:
+	    for i in tl:
 		    print i[0] + " " + str(i[1]) + "s"
 
 if __name__ == "__main__":
